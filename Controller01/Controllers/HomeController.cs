59cdf06b6ea66e540e4627d6ddc5b891a0f64295
NaviD Kian <n.soleimani@haierasa.ir>
2020-01-24 22:57:30 +0330

    Add project files.

---
+++ Controller01/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controller01.Controllers
+{
+    /// <summary>
+    /// کنترلر خودش یک کلاس است
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// حالت اول
+        /// کاربر درخواست می دهد
+        /// از کنترلر جواب می گیرد
+        /// انواع نام برده از اکشن ریزالت ارث بری می کنند
+        /// این اکشن حتما باید پارامتری اینتیجر به عنوان آی دی داشته باشد 
+        /// اگر نداشته باشد ارور مناسب می دهد
+        /// وقتی از علامت سوال  بعد از نوع اسنفاده می کنیم
+        /// آن متغیر  دارای پراپرتی هز ولیو می شود
+        /// هز ولیو جهت چک کردن اینکه آیا مقدار دارد یا خیر
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult session01_08(int? id)
+        {
+            string strContent = string.Empty;
+            if (id.HasValue)
+            {
+                strContent = id.Value.ToString();
+            }
+            else
+            {
+                strContent = "Id Has A null Value";
+            }
+            return (Content(strContent));
+        }
+    }
+}
---
+++ Controller01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Controller01.MvcApplication" Language="C#" %>
